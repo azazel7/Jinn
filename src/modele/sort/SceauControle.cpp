2b8ac496923f10d281e29c3025511ecaa9bf9692
Azazel <mel@statler.world>
2013-04-07 18:05:29 -0400

    Retrait des affichages

--- src/modele/sort/SceauControle.cpp
+++ src/modele/sort/SceauControle.cpp
@@ -7,24 +7,19 @@ SceauControle::SceauControle(): Sort("Sceau de Controle", 1, false, 5, 100, 1, 0
 
 void SceauControle::appliquerSortSurCase(Case &cible)
 {
-    cout << "propri " << this->proprietaire << endl;
-    cout << "cible : " << cible << endl;
     cible.setProprietaire(this->proprietaire); //On modifie le proprietaire
 }
 
 void SceauControle::modifierSuivantOrigine(Case const& origine)
 {
-    cout << "Rien à modifier selon origine" << endl;
 }
 
 void SceauControle::modifierSuivantProprietaire()
 {
-    cout << "Modification selon : " << proprietaire->getNom() << endl;
 }
 
 void SceauControle::retirerDeCase(Case &cible)
 {
-
 }
 
 string SceauControle::description()
