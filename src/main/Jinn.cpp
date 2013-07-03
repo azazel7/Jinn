70834c02f371e0119f597ecc1df2af55433dc868
Azazel <mel@statler.world>
2013-07-03 12:36:49 -0400

    Ajout d'un traitement de signal.

--- src/main/Jinn.cpp
+++ src/main/Jinn.cpp
@@ -11,17 +11,39 @@
 
 using namespace std;
 
+ReceptionServeur *serveur = NULL;
+void traitement_sigint(int sig)
+{
+    cout << "Reception du signal " << sig << endl;
+    if(sig == SIGINT)
+    {
+        if(serveur != NULL)
+        {
+            serveur->finirServeur();
+            serveur->fermerServeur();
+            delete serveur;
+            serveur = NULL;
+            GestionnaireLogger::viderRegistre();
+            Position::libererPositions();
+
+            exit(0);
+        }
+    }
+}
 int main()
 {
         //TODO traitement des signaux
+    signal(SIGINT, &traitement_sigint);
     Logger *logger = new LoggerFileDescriptor(1, false);
     GestionnaireLogger::ajouterRegistre(logger);
     Partie *p = new Partie("p1", 2, 2);
     p->initialiser();
-    ReceptionServeur serveur(p);
-    serveur.initialiserServeur();
-    serveur.miseEnEcoute();
-    serveur.fermerServeur();
+    serveur = new ReceptionServeur(p);
+    serveur->initialiserServeur();
+    serveur->miseEnEcoute();
+    serveur->fermerServeur();
+    delete serveur;
+    serveur = NULL;
     GestionnaireLogger::viderRegistre();
     Position::libererPositions();
     //TODO fermer reception avec fermeture des sockets
