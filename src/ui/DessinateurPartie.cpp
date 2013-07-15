0e79c5619cc64cb96a577cbf3e74ba20f219b950
Azazel <mel@statler.world>
2013-07-15 16:09:00 -0400

    Affichage plus agréable.
    Le terrain s'affiche, mais il sera à revoir un brin pour plus d'indication.
    Les info de la case est du joueur s'affiche, mais il faudra éffacer les -1.
    Les messages s'affiche correctement, il faudra penser à prévoir un cadre pour le message en cour.

--- src/ui/DessinateurPartie.cpp
+++ src/ui/DessinateurPartie.cpp
@@ -37,7 +37,12 @@ void DessinateurPartie::dessinerJoueurs(int hauteur, int largeur)
             wprintw(win, "Nom : %s   Equipe : %s", it->first.c_str(), joueur->getNomEquipe().c_str());
             wmove(win, i + 1, 1); //1 pour éviter d'empiéter sur la bordure
             wprintw(win, "Mana : %d/%d  Gain : %d", joueur->getManaActuel(), joueur->getManaMaximum(), joueur->getGainInitialMana());
-            i += 3; //2 ligne d'écriture et une pour l'espace
+            if(joueur == this->partie->getJoueurCourant())
+            {
+                wmove(win, i + 2, 1); //1 pour éviter d'empiéter sur la bordure
+                wprintw(win, "** Tour **");
+            }
+            i += 4; //lignes d'écriture et une pour l'espace
         }
     }
     //TODO faire la limite d'affichage s'il y a trop de joueur
@@ -51,12 +56,13 @@ void DessinateurPartie::dessinerMessage(int hauteur, int largeur)
     int l_win = ((4*largeur)/5);
     int i = 1;
     WINDOW * win = subwin(stdscr, h_win, l_win, (4*hauteur)/5, largeur/5);
+    wborder(win, '|', '|', '-', '-', '+', '+', '+', '+');
     wrefresh(win);
     list<pair<string, string> > listeMessage = this->partie->getListeMessage();
     for(list<pair<string, string> >::iterator it = listeMessage.begin(); it != listeMessage.end(); it++)
     {
         wmove(win, i, 1); //1 pour éviter d'empiéter sur la bordure
-        wprintw(win, "%s  |%s", it->first.c_str(), it->second.c_str());
+        wprintw(win, "%s | %s", it->first.c_str(), it->second.c_str());
         if(i > 5)
         {
             break;
@@ -82,15 +88,13 @@ void DessinateurPartie::dessinerPlateau(int hauteur, int largeur)
     int y_origine = 0;
     int i = 1;
     WINDOW * win = subwin(stdscr, h_win, l_win, 0, largeur/5);
-    wrefresh(win);
-
-    for(int x = x_origine; x < x_origine + l_vision_case; x+= l_case)
+    for(int x = x_origine; x < x_origine + l_vision_case; x++)
     {
-        for(int y = y_origine; y < y_origine + h_vision_case; y+= h_case)
+        for(int y = y_origine; y < y_origine + h_vision_case; y++)
         {
             if(this->partie->getCase(Position::fabriquePosition(x, y)) != NULL)
             {
-                wmove(win, y, x); //1 pour éviter d'empiéter sur la bordure
+                wmove(win, y*h_case, x*l_case); //1 pour éviter d'empiéter sur la bordure
                 if(this->positionCourante == Position::fabriquePosition(x, y))
                 {
                     wattron(win, COLOR_PAIR(1));
@@ -103,9 +107,39 @@ void DessinateurPartie::dessinerPlateau(int hauteur, int largeur)
             }
         }
     }
+    wrefresh(win);
 }
 
 void DessinateurPartie::dessinerCaseCourante(int hauteur, int largeur)
 {
-
+    int h_win = (4*hauteur)/5;
+    int l_win = largeur/5;
+    WINDOW * win = subwin(stdscr, h_win, l_win, 0, (4*largeur)/5);
+    wborder(win, '|', '|', '-', '-', '+', '+', '+', '+');
+    Case* caseCourante = this->partie->getCase(this->positionCourante);
+    if(caseCourante != NULL)
+    {
+        wmove(win, 1, 1); //1 pour éviter d'empiéter sur la bordure
+        wprintw(win, "[%d;%d]", this->positionCourante->getX(), this->positionCourante->getY());
+        wmove(win, 2, 1); //1 pour éviter d'empiéter sur la bordure
+        wprintw(win, "Def Init : %d", caseCourante->getDefenseInitiale());
+        wmove(win, 3, 1); //1 pour éviter d'empiéter sur la bordure
+        wprintw(win, "Defense : %d/%d", caseCourante->getDefenseActuelle(), caseCourante->getDefenseReel());
+        wmove(win, 4, 1); //1 pour éviter d'empiéter sur la bordure
+        wprintw(win, "Apport Mana : %d", caseCourante->getApportMana());
+        wmove(win, 5, 1); //1 pour éviter d'empiéter sur la bordure
+        wprintw(win, "Regeneration : %d", caseCourante->getRegenerationDefense());
+        wmove(win, 6, 1); //1 pour éviter d'empiéter sur la bordure
+        wprintw(win, "Proprietaire : %s", caseCourante->getNomProprietaire().c_str());
+        wmove(win, 7, 1); //1 pour éviter d'empiéter sur la bordure
+        wprintw(win, "  --- Sort ---  ");
+        int i = 8;
+        list<pair<int, Sort*> > listeSort = caseCourante->getListSort();
+        for(list<pair<int, Sort*> >::iterator it = listeSort.begin(); it != listeSort.end(); it++)
+         {
+            wmove(win, 8, 1); //1 pour éviter d'empiéter sur la bordure
+            wprintw(win, "%s (%d)", it->second->getNom().c_str(), it->first);
+        }
+    }
+    wrefresh(win);
 }
