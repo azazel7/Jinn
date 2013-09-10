16a701b160be28f52e2a773fceea804b420e53f0
Azazel <mel@statler.world>
2013-09-10 01:14:40 -0400

    Ajout d'un attribut pour infliger les dégats et retrait d'une classe devenue inutile.

--- include/sort/actionApplique/AppliqueDegat.h
+++ include/sort/actionApplique/AppliqueDegat.h
@@ -2,10 +2,13 @@
 #define AppliquerDegat_h
 
 #include "sort/SortAppliquerSurCase.h"
+#include "Case.h"
 
 class AppliqueDegat: public SortAppliquerSurCase
 {
+    int degat;
 public:
+    AppliqueDegat(int degat);
     virtual void appliquerSortSurCase(Case &cible, Sort* sortExecutant);
     virtual void retirerSortDeCase(Case &cible, Sort* sortExecutant);
 };
--- src/modele/sort/actionApplique/AppliqueDegat.cpp
+++ src/modele/sort/actionApplique/AppliqueDegat.cpp
@@ -1,9 +1,13 @@
 #include "sort/actionApplique/AppliqueDegat.h"
-#include "Case.h"
+
+AppliqueDegat::AppliqueDegat(int degat)
+{
+    this->degat = degat;
+}
 
 void AppliqueDegat::appliquerSortSurCase(Case &cible, Sort* sortExecutant)
 {
-    cible.modifierDefense(-sortExecutant->getAttaque());
+    cible.modifierDefense(-this->degat);
 }
 void AppliqueDegat::retirerSortDeCase(Case &cible, Sort* sortExecutant)
 {
